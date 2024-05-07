8b2e79e60fd46434de5b71d1865e3b249b03f8d7
choemxin1 <160872427+choemxin1@users.noreply.github.com>
2024-05-07 10:32:16 +0700

    Update homeController.js

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,7 +3,7 @@ import CRUDService from "../services/CRUDService";
 
 let getHomePage = async (req, res) => {
     try {
-        // let data = await db.User.findAll();
+         let data = await db.User.findAll();
         return res.render('homepage.ejs', {
             data: JSON.stringify({})
         });
